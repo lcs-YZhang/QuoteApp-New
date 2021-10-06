d0b05c58eb8177defd75141dcc15f81d88d45cc3
Steven Zhang <YZhang@lcs.on.ca>
2021-10-06 10:16:38 -0400

    Initial Commit

---
+++ QuoteApp/QuoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteAppApp.swift
+//  QuoteApp
+//
+//  Created by Steven Zhang on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct QuoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
