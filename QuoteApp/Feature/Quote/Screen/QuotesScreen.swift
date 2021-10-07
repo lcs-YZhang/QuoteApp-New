1301ab4472b8b2b948637e4d535b729d5e9e524c
Steven Zhang <YZhang@lcs.on.ca>
2021-10-07 09:14:16 -0400

    Fix problems that occur in the QuotesScreen

--- QuoteApp/Feature/Quote/Screen/QuotesScreen.swift
+++ QuoteApp/Feature/Quote/Screen/QuotesScreen.swift
@@ -29,7 +29,20 @@ struct QuotesScreen: View {
                 .foregroundColor(.black)
             }
         }
+    }
+    
+    private func makeAttributedString(title: String, label: String) -> AttributedString
+        {
+        var string = AttributedString("\(title): \(label)")
+        string.foregroundColor = .black
+        string.font = .system(size: 16, weight: .bold)
         
+        if let range = string.range(of: label) {
+            string[range].foregroundColor = .black.opacity(0.8)
+            string[range].font = .system(size: 16, weight: .regular)
+            
+        }
+        return string
     }
 }
 
